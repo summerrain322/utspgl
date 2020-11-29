24d8b3fe510e834cee2967e23271d4c76af3c77d
summerrain322 <32408008+summerrain322@users.noreply.github.com>
2020-11-29 22:55:46 +0700

    UTS
    ArgaSenaMuhammad_160

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player.transform.position);
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+	}
+    void OnCollisionStay(Collision Obj)
+    {
+        if(Obj.gameObject.name == "Player")
+        {
+            speed = 0f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	
+	
+	//Membuat player bergerak dengan tombol W A S D
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0,- 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(10 * Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-10 * Time.deltaTime,0,0);
+        }
+	}
+}
